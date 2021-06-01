139ae0ab637f35372d9a7c4340e27bf4e670fdf0
Mcl99XL <mclsantos0900@gmail.com>
2021-06-01 00:25:59 -0300

    wip

--- data/spells/scripts/monster/triple_fire.lua
+++ data/spells/scripts/monster/triple_fire.lua
@@ -1,7 +1,7 @@
 local combat = Combat()
 combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, 120)
-combat:setFormula(COMBAT_FORMULA_DAMAGE, -8000, 0, -8000, 0)
+combat:setFormula(COMBAT_FORMULA_DAMAGE, -5000, 0, -5000, 0)
 
 function onCastSpell(creature, variant)
 	local target = Creature(variant:getNumber())
